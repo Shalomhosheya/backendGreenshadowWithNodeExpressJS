ee6925ea9b0ed8b5602de409d690a95a69aa7a03
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-02-19 19:56:50 +0530

    intial comit

--- controllers/cropController.ts
+++ controllers/cropController.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import {Crop} from "../model/crop";
 import { extractImages } from "../util/Apputil";
-import { AddCrop } from "../services/cropService";
+import { AddCrop,UpdateCrop,DeleteCrop,GetAllCrops } from "../services/cropService";
 const router = express.Router();
 
 router.post('/addCrop', async (req, res) => {
@@ -16,4 +16,40 @@ router.post('/addCrop', async (req, res) => {
         res.status(400).send("Error during crop");
     }
 });
+router.put('/updateCrop/:cropID', async (req, res) => {
+    console.log("Received Update Crop : ", req.body);
+    const cropCode: string = req.params.cropID;
+    const crop: Crop = req.body;
+
+    try {
+        await UpdateCrop(cropCode, crop);
+        res.status(200).send("Crop Updated");
+    } catch (err) {
+        console.log("Error during crop updating : ", err);
+        res.status(400).send("Error during Crop");
+    }
+})
+
+router.delete('/deleteCrop/:cropID', async (req, res) => {
+    console.log("Received Deleted crop code");
+    const cropCode: string = String(req.params.cropID);
+    try {
+        await DeleteCrop(cropCode);
+        res.status(200).send("Crop Deleted")
+    } catch (err) {
+        console.log("Error during deleting crop :", err);
+        res.status(400).send("Error during crop");
+    }
+})
+
+
+router.get('/viewAllCrop', async (req, res) => {
+    console.log("All crop details are retrieved");
+    try {
+        const crops = await GetAllCrops();
+        res.json(crops)
+    } catch (err) {
+        console.log("error during getting crops")
+    }
+})
 export default router;
--- services/cropService.ts
+++ services/cropService.ts
@@ -21,3 +21,48 @@ export async function AddCrop(crop:Crop){
         console.log("Error during crop adding : ", err)
     }
 }
+export async function UpdateCrop(cropID: string, crop: Crop) {
+    try {
+        const existingCrop = await prisma.crop.findUnique({
+            where: { cropID: cropID },
+        });
+
+        if (!existingCrop) {
+            console.log(`Crop with cropCode: ${cropID} not found.`);
+            return null;
+        }
+
+        const cropSeason = String(crop.season);
+        const cropUpdate = await prisma.crop.update({
+            where: { cropID: cropID },
+            data: {
+                cropName: crop.cropName,
+                cropImage: crop.cropImage,
+                scientificName: crop.scientificName,
+                category: crop.category,
+                season: cropSeason,
+            },
+        });
+
+        console.log("Crop Updated ", cropUpdate);
+        return cropUpdate;
+    } catch (err) {
+        console.error("Error during crop updating:", err);
+    }
+}
+export async function DeleteCrop(cropID:string){
+    try {
+        await prisma.crop.delete({
+            where:{cropID:cropID}
+        })
+    }catch (err){
+        console.log("Error during customer deleting : ", err)
+    }
+}
+export async function GetAllCrops(){
+    try{
+        return await prisma.crop.findMany();
+    }catch (err){
+        console.log("Error getting crops : ",err)
+    }
+}
